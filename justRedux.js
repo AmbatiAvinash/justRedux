c81c57791bec9727180a78101256dfbc971c95bb
Ambati Avinash <avi.ambati07@gmail.com>
2020-03-09 15:08:28 +0530

    Incrementing age using redux only

---
+++ justRedux.js
@@ -0,0 +1,33 @@
+const {createStore} = require('redux');
+
+const initialState = {
+    age: 21
+}
+
+const myReducer = (state = initialState, action) => {
+    const newState = {...state}
+
+    if(action.type === 'ADD'){
+        newState.age += 1
+    }
+    else if (action.type === 'SUBTRACT'){
+        newState.age -= 1
+    }
+
+    return newState;
+}
+
+const store = createStore(myReducer);
+
+store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch({type:'ADD'});
+
+store.dispatch({type:'SUBTRACT'});
+
+// console.log(store.getState());
+
+
+
